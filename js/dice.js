94e52481b563cbc632043f03c83fa19d0087f6f9
micah-rose <micah.r.lund@gmail.com>
2018-12-06 17:58:47 -0700

    started to set up the HTML and planned out the JavaScript functions with tests to make sure when buttons are clicked, they properly call the function.

---
+++ js/dice.js
@@ -0,0 +1,16 @@
+//TO DO's:
+//Create function for roll button
+//Create function for reset button
+
+function rollDie(){
+    console.log("Die have been rolled");
+}
+
+
+function resetRolls(){
+    console.log("Die have been reset");
+}
+
+function dieBatch(){
+    console.log("Batch box has been checked");
+}
